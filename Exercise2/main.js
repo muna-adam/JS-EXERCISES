fd62e73236293d326ec4a93a03be9926321a061a
muna-adam <maymuunaadan252@gmail.com>
2026-02-23 12:08:42 +0300

    Exercise 2

---
+++ Exercise2/main.js
@@ -0,0 +1,12 @@
+a = 15
+b = "15"
+
+
+console.log(a == b)
+console.log(a != b)
+console.log(a === b)
+console.log(a !== b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
