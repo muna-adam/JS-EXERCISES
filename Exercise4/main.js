7be8b9558459d634c61de4d7dd5f438fd1ba3066
muna-adam <maymuunaadan252@gmail.com>
2026-03-01 19:56:41 +0300

    exercise4

---
+++ Exercise4/main.js
@@ -0,0 +1,21 @@
+
+
+function add(a,b){
+
+    return a+b;
+}
+
+console.log(add(10,20));
+console.log(add(11,60));
+console.log(add(3,6));
+console.log(add(100,20));
+
+const sum = function (a,b){
+    return a+b;
+}
+
+console.log(sum(1,3));
+console.log(sum(11,30));
+console.log(sum(22,50));
+console.log(sum(10,60));
+
