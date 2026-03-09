386d4c1ad368b53622fa5b1eda01b707e24c20c6
muna-adam <maymuunaadan252@gmail.com>
2026-03-09 11:26:15 +0300

    Exercise7

---
+++ Exercise7/main.js
@@ -0,0 +1,15 @@
+let colors = ["green", "blue", "black"]
+
+colors.push ("yellow")
+console.log(colors)
+
+colors.pop ()
+console.log(colors)
+
+colors.shift()
+console.log(colors)
+
+colors.unshift("red")
+console.log(colors)
+
+console.log(colors.length)
