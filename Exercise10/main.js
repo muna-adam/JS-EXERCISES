9aae4520cd902b8f88c7d710cd9221a95e0d09db
muna-adam <maymuunaadan252@gmail.com>
2026-03-17 12:20:05 +0300

    Exercise10

---
+++ Exercise10/main.js
@@ -0,0 +1,28 @@
+
+
+
+let library = [
+    {
+        title: "atomic habit",
+        author: "james clear",
+        year: 2018,
+
+    }, 
+    
+    {
+        title: "The 7 Habits of Highly Effective People",
+        author: "Stephen R. Covey",
+        year: 1989,
+
+    } ,
+
+    {
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        year: 1997,
+
+    } ,
+]
+
+console.log(library[0].title)
+console.log(library[1].author)
