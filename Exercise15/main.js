8525e9bce1064600bec00151920271fbe0491d2c
muna-adam <maymuunaadan252@gmail.com>
2026-03-28 10:57:05 +0300

    Exercise15

---
+++ Exercise15/main.js
@@ -0,0 +1,22 @@
+
+
+
+const peoples = [
+    {name: "mariya", age: 20, city:"boosaaso"},
+    {name: "caisha", age: 38, city: "hargaysa"},
+    {name: "xasan", age: 27, city: "galkacyo"},
+]
+
+for(let people of peoples){
+   
+    console.log("person")
+
+
+for( let key in people){
+    console.log(key + ": " + people[key])
+}
+
+}
+
+
+
