d291d2d0a5f893a618e329b164f843adbdbcfbd5
muna-adam <maymuunaadan252@gmail.com>
2026-03-29 11:27:13 +0300

    Exercise17

---
+++ Exercise17/main.js
@@ -0,0 +1,21 @@
+
+let tempreture = 25
+
+if (tempreture >= 25){
+
+    console.log("hot")
+}
+
+else if(tempreture >= 15 && tempreture < 25 ){
+
+    console.log("warm")
+}
+
+else if(tempreture >= 0 &&  tempreture < 15 ){
+
+    console.log("cold")
+}
+
+else{
+    console.log("very cold")
+}
