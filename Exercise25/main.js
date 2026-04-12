b62e5bececa80342129d918603137552fff143f8
muna-adam <maymuunaadan252@gmail.com>
2026-04-12 11:27:15 +0300

    Exercise25

---
+++ Exercise25/main.js
@@ -0,0 +1,17 @@
+
+
+let numbers = [1,2,3];
+
+let allnumbers = [...numbers, 4,5,6];
+
+console.log(allnumbers);
+
+
+
+
+
+function kudhufo (...numbers){
+    return numbers.reduce((acc, num)  => acc*num, 1)
+}
+
+console.log(kudhufo(13,34))
