9fd820ccec0c37708d897df9499ad1dba46648ad
muna-adam <maymuunaadan252@gmail.com>
2026-04-13 11:18:20 +0300

    Exercise26

--- Exercise26/main.js
+++ Exercise26/main.js
@@ -1,24 +1,18 @@
+function userdata() {
+  alert("welcome back");
 
-function userdata (){
-
-    alert("welcome back");
-
-    return {name: "ali", id: 2345, city:"bosaso"}
+  return { name: "ali", id: 2345, city: "bosaso" };
 }
 
-const user= userdata();
-console.log(user); 
+const user = userdata();
+console.log(user);
 
-
-
-
-function dibudhac(callback){
-setTimeout(()=> {
-    console.log("kadib 2 second soo muuqo")
-     }, 2000)
- }
+function dibudhac(callback) {
+  setTimeout(() => {
+    console.log("kadib 2 second soo muuqo");
+  }, 2000);
+}
 
 dibudhac();
 
 console.log("kusoo dhawoow gacmo furan");
-
