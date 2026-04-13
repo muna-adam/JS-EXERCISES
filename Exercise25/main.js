a9c80c8bc6707509d5c7eee8d37aaec3c2f7e0a5
muna-adam <maymuunaadan252@gmail.com>
2026-04-13 11:16:07 +0300

    Exercise26

--- Exercise25/main.js
+++ Exercise25/main.js
@@ -1,17 +1,11 @@
+let numbers = [1, 2, 3];
 
-
-let numbers = [1,2,3];
-
-let allnumbers = [...numbers, 4,5,6];
+let allnumbers = [...numbers, 4, 5, 6];
 
 console.log(allnumbers);
 
-
-
-
-
-function kudhufo (...numbers){
-    return numbers.reduce((acc, num)  => acc*num, 1)
+function kudhufo(...numbers) {
+  return numbers.reduce((acc, num) => acc * num, 1);
 }
 
-console.log(kudhufo(13,34))
+console.log(kudhufo(13, 34));
---
+++ Exercise26/main.js
@@ -0,0 +1,24 @@
+
+function userdata (){
+
+    alert("welcome back");
+
+    return {name: "ali", id: 2345, city:"bosaso"}
+}
+
+const user= userdata();
+console.log(user); 
+
+
+
+
+function dibudhac(callback){
+setTimeout(()=> {
+    console.log("kadib 2 second soo muuqo")
+     }, 2000)
+ }
+
+dibudhac();
+
+console.log("kusoo dhawoow gacmo furan");
+
